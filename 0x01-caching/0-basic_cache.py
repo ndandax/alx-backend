dcb2653559d907a25b39fea4a3f47342db882e30
ndandax <sebandandala99@gmail.com>
2023-10-25 21:44:08 +0300

    caching

---
+++ 0x01-caching/0-basic_cache.py
@@ -0,0 +1,24 @@
+#!/usr/bin/python3
+""" module inherits from basecaching
+"""
+BaseCaching = __import__('base_caching').BaseCaching
+
+
+class BasicCache(BaseCaching):
+    """inherits from base caching"""
+    def __init__(self):
+        """intializer"""
+        super().__init__()
+    def put(self, key, item):
+        """assigning values and keys to dictionary"""
+        if key == None or item == None:
+            pass
+        else:
+            self.cache_data[key] = item
+
+    def get(self, key):
+        """getting key"""
+        if key is None:
+            pass
+        else:
+            return self.cache_data.get(key)
