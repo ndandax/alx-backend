d51b3a39a1ae764b7aebde175719a7f8e1d52339
ndandax <sebandandala99@gmail.com>
2023-10-25 22:01:55 +0300

    caching-1

---
+++ 0x01-caching/1-fifo_cache.py
@@ -0,0 +1,33 @@
+#!/usr/bin/env python3
+"""First-In First-Out caching module.
+"""
+from base_caching import BaseCaching
+
+
+class FIFOCache(BaseCaching):
+    """Represents an object that allows storing and
+    retrieving items from a dictionary with a FIFO
+    removal method when the limit is reached.
+    """
+    def __init__(self):
+        """Initializes the cache.
+        """
+        super().__init__()
+
+    def put(self, key, item):
+        """Adds an item in the cache.
+        """
+        if key is not None and item is not None:
+            if len(self.cache_data) >= BaseCaching.MAX_ITEMS:
+                oldest_key = next(iter(self.cache_data))
+                del self.cache_data[oldest_key]
+                print(f"DISCARD: {oldest_key}\n")
+            self.cache_data[key] = item
+
+    def get(self, key):
+        """Retrieves an item by key.
+        """
+        if key is not None:
+            return self.cache_data.get(key)
+        else:
+            return None
