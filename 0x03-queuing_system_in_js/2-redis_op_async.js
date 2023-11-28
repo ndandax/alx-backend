9b5004a7984c4783fe15244514c953c6bb24f9a9
ndandax <sebandandala99@gmail.com>
2023-11-28 21:08:48 +0300

    queque

---
+++ 0x03-queuing_system_in_js/2-redis_op_async.js
@@ -0,0 +1,35 @@
+import redis from 'redis';
+import { promisify } from 'util';
+import { createClient } from 'redis';
+import { truncateSync } from 'fs';
+
+const client = redis.createClient();
+
+client.on('error', (err) => {
+  console.log(`Redis client not connected to the server: ${err.message}`);
+});
+
+client.on('connect', () => {
+  console.log('Redis client connected to the server');
+});
+
+function setNewSchool(schoolName, value) {
+  client.set(schoolName, value, redis.print);
+}
+
+const asyncGet = promisify(client.get).bind(client);
+
+async function displaySchoolValue(schoolName) {
+    try {
+      const value = await asyncGet(schoolName);
+      console.log(value);
+      client.quit();
+    } catch (err) {
+      console.error(err);
+      client.quit();
+    }
+}
+
+displaySchoolValue('Holberton');
+setNewSchool('HolbertonSanFrancisco', '100');
+displaySchoolValue('HolbertonSanFrancisco');
